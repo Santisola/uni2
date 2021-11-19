1486e3a597620e49a9920b55250d913438066b6a
Santi <santiago.i@knownonline.com>
2021-11-18 21:26:51 -0300

    comentado de header y de die

--- api/public/index.php
+++ api/public/index.php
@@ -1,6 +1,6 @@
 <?php
-header('Access-Control-Allow-Origin: http://localhost:8080');
-die;
+/* header('Access-Control-Allow-Origin: http://localhost:8080');
+die */
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
