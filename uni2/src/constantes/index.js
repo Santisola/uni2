6ff4412f67af090e400e8253c0cc67d8ab077ccc
Santi <santiago.i@knownonline.com>
2022-01-10 10:32:10 -0300

    Ajustes para buildeo de cordova

--- uni2/src/constantes/index.js
+++ uni2/src/constantes/index.js
@@ -1,6 +1,6 @@
-/* const API = 'http://isola-pruebas.atwebpages.com/unidos/api/public/api';
-const IMG_PATH = 'http://isola-pruebas.atwebpages.com/unidos/api/public/imgs/mascotas/'; */
-const API = 'http://localhost/uni2/api/public/api';
-const IMG_PATH = 'http://localhost/uni2/api/public/imgs/mascotas/';
+const API = 'https://eproject.com.ar/dav-isola/unidos/public/api';
+const IMG_PATH = 'https://eproject.com.ar/dav-isola/unidos/public/imgs/mascotas/';
+/* const API = 'http://localhost/uni2/api/public/api';
+const IMG_PATH = 'http://localhost/uni2/api/public/imgs/mascotas/'; */
 
 export {API, IMG_PATH};
