16bbe629fd76f57924a1b42332f1ebde971f0cad
Matias Bubello <matt.bubello@gmail.com>
2022-05-26 21:04:10 -0300

    La api ahora guarda imágenes

--- api/app/Http/Controllers/VerificadosController.php
+++ api/app/Http/Controllers/VerificadosController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class VerificadosController extends Controller
 {
@@ -42,7 +43,7 @@ class VerificadosController extends Controller
         ]);
     }
 
-    public function register(Request $request, Client $client)
+    public function register(Request $request, Client $client) : JsonResponse
     {
         $validator = Validator::make($request->all(),Verificados::$reglas,Verificados::$mensajesDeError);
 
@@ -91,5 +92,107 @@ class VerificadosController extends Controller
             ]);
         }
 
+    }
+
+    /**
+     * Edita los datos del usuario
+     * @param Request $request
+     * @param int $usuario
+     * @return JsonResponse
+     */
+    public function update(Request $request,int $usuario) : JsonResponse
+    {
+        $validator = Validator::make($request->all(),[
+            'email' => 'required|email',
+            'imagen' => 'image|required|max:10000',
+            'telefono' => 'required|unique:usuarios_verificados,telefono',
+            'password' => 'required'
+        ],[
+            'email.required'=>'El email es obligatorio',
+            'email.email'=>'El email debe ser de un formato válido (nombre@dominio.extension)',
+            'email.unique'=>'El email que intenta ingresar ya existe en nuestra base de datos',
+            'password.required'=>'La contraseña es obligatoria',
+            'imagen.image' => 'El formato de la imagen no es valido. Intente en un formato .jpeg, .jpg o .png',
+            'imagen.required' => 'La imagen en esta sección es obligatoria',
+            'imagen.max' => 'La imagen es muy pesada, intente una menor tamaño',
+            'telefono.required' => 'El teléfono en esta instancia es obligatorio',
+            'telefono.unique' => 'Ese número de teléfono ya se encuentra registrado'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'data' => $validator->messages()
+            ]);
+        }
+
+        try {
+            $user = Verificados::findOrFail($usuario);
+            $file_name = Str::random(35) . '_' . $request->imagen->getClientOriginalName();
+            $request->imagen->move(public_path('/imgs/perfiles'),$file_name);
+            $path = "public/images/perfiles/$file_name";
+
+            $user->imagen = $path;
+            $user->telefono = $request->telefono;
+            $user->email = $request->email;
+            $request->password === '' ?? $user->password = $request->password;
+
+            $user->save();
+
+            return response()->json([
+               'success'=> true,
+               'data' => $user
+            ]);
+
+        } catch (\Exception $exception) {
+            return response()->json([
+               'success' => false,
+               'data' => 'Ocurrió un error al momento de modificar sus datos' . $exception
+            ]);
+        }
+    }
+
+    public function completeData(Request $request, int $usuario)
+    {
+        $validator = Validator::make($request->all(),[
+            'imagen' => 'image|required|max:10000',
+            'telefono' => 'required|unique:usuarios_verificados,telefono',
+        ],[
+            'imagen.image' => 'El formato de la imagen no es valido. Intente en un formato .jpeg, .jpg o .png',
+            'imagen.required' => 'La imagen en esta sección es obligatoria',
+            'imagen.max' => 'La imagen es muy pesada, intente una menor tamaño',
+            'telefono.required' => 'El teléfono en esta instancia es obligatorio',
+            'telefono.unique' => 'Ese número de teléfono ya se encuentra registrado'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'data' => $validator->messages()
+            ]);
+        }
+
+        try {
+            $user = Verificados::findOrFail($usuario);
+            $file_name = Str::random(35) . '_' . $request->imagen->getClientOriginalName();
+            $request->imagen->move(public_path('/imgs/perfiles'),$file_name);
+            $path = "public/images/perfiles/$file_name";
+
+            $user->imagen = $path;
+            $user->telefono = $request->telefono;
+
+            $user->save();
+
+            return response()->json([
+                'success'=> true,
+                'data' => $user
+            ]);
+
+        } catch (\Exception $exception) {
+            return response()->json([
+                'success' => false,
+                'data' => 'Ocurrió un error al momento de modificar sus datos' . $exception
+            ]);
+        }
     }
 }
--- api/routes/api.php
+++ api/routes/api.php
@@ -50,3 +50,5 @@ Route::post('/registro', [AuthController::class, 'registrar']);
  */
 Route::post('/verificado/login', [VerificadosController::class,'login']);
 Route::post('/verificado/register', [VerificadosController::class,'register']);
+Route::put('/verificado/{usuario}/update', [VerificadosController::class,'update']);
+Route::put('/verificado/{usuario}/complete', [VerificadosController::class,'completeData']);
