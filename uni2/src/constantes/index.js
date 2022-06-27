7bce4d353c682806f2d4b77f558a4fc39bcac6c5
Santi <santiago.i@knownonline.com>
2022-06-27 19:21:11 -0300

    Agregado de eventos en el mapa y de opcion en el nav para filtrar

--- uni2/src/constantes/index.js
+++ uni2/src/constantes/index.js
@@ -2,7 +2,8 @@
 const IMG_PATH = 'https://eproject.com.ar/dav-isola/unidos/public/imgs/mascotas/'; */
 const API = 'http://localhost/uni2/api/public/api';
 const IMG_PATH = 'http://localhost/uni2/api/public/imgs/mascotas/';
+const EVENTOS_IMG_PATH = 'http://localhost/uni2/api/';
 const MAPBOX_PUBLIC_TOKEN = "pk.eyJ1Ijoic2FudGlzb2xhIiwiYSI6ImNsMTFhZ2U4NTJweTQzaXJweHA3NnNscG4ifQ.F5dFKS4tv7ThSzmwdCBUIw";
 const HERE_API_KEY = 'VD4PChdAlGTj7MKuMCx77Bpy-RY1zQMrpOTvCKpQHY4';
 
-export {API, IMG_PATH, MAPBOX_PUBLIC_TOKEN, HERE_API_KEY};
+export {API, IMG_PATH, EVENTOS_IMG_PATH, MAPBOX_PUBLIC_TOKEN, HERE_API_KEY};
