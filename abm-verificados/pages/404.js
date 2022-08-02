0058a302c0417544032540564760a2e24d42caf8
Matias Bubello <matt.bubello@gmail.com>
2022-08-01 22:24:20 -0300

    Updates

--- abm-verificados/pages/404.js
+++ abm-verificados/pages/404.js
@@ -1,7 +1,6 @@
 import Layout from "../layouts/layout";
 import Link from "next/link";
 import Styles from '../styles/404.module.css'
-import Image from "next/image";
 import { useEffect, useState } from "react";
 import {useRouter} from "next/router";
 import UsuarioEliminado from "../components/UsuarioEliminado";
@@ -35,7 +34,8 @@ export default function Error404() {
             ) }
             <h2 className={"sr-only"}>Página no encontrada</h2>
             <p className={"text-5xl text-center font-semibold"}>¡Ups!</p>
-            <Image layout={"responsive"} width={330} height={330} src={"/imgs/404.svg"} alt={"Error"} />
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img width={330} height={330} src={"/imgs/404.svg"} alt={"Error"} className={"mx-auto"} />
             <p className={"mt-5 text-2xl font-semibold text-center"}>Parece que la sección que busca no existe</p>
             <Link href={"/"}>
                 <a className={Styles.error}>Click aquí para volver</a>
