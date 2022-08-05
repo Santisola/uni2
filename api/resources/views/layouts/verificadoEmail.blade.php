8024be0b18fd919cfab44cbd735495255ff461c4
Matias Bubello <matt.bubello@gmail.com>
2022-08-05 11:39:17 -0300

    mailing español

--- api/resources/views/layouts/verificadoEmail.blade.php
+++ api/resources/views/layouts/verificadoEmail.blade.php
@@ -4,13 +4,13 @@
  */
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html lang="en">
+<html lang="es">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>Grids Master Template</title>
+    <title>Recuperación de contraseña</title>
 
     <style type="text/css">
         @import url({{ asset('css/fonts/stylesheet.css') }});
